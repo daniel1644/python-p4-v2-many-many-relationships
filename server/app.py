4cfe49cf1b69ec657d4c9a469a0ab5ef9277494a
daniel beyene <dazimore@gmail.com>
2024-07-18 12:03:50 +0300

    Many-To-Many Relationships : Code-Along (CodeGrade)

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 
-from models import db
+from models import db 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
